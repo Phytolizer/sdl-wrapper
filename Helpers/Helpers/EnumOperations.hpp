98596405fd46c8422ee27cdba367d25dfb6b9ace
kyle.coffey <kyle.coffey@tamu.edu>
2020-11-11 16:55:32 -0600

    Conform to spec for enum operations

--- Helpers/Helpers/EnumOperations.hpp
+++ Helpers/Helpers/EnumOperations.hpp
@@ -8,9 +8,56 @@ template <typename E> struct EnableBitmaskOperations
 };
 
 template <typename E>
-requires(EnableBitmaskOperations<E>::enable) constexpr E operator|(E lhs, E rhs)
+requires(EnableBitmaskOperations<E>::enable) inline constexpr E operator|(E lhs,
+                                                                          E rhs)
 {
     using underlying_type = typename std::underlying_type<E>::type;
     return static_cast<E>(static_cast<underlying_type>(lhs) |
                           static_cast<underlying_type>(rhs));
+}
+
+template <typename E>
+requires(EnableBitmaskOperations<E>::enable) inline constexpr E operator&(E lhs,
+                                                                          E rhs)
+{
+    using underlying_type = typename std::underlying_type<E>::type;
+    return static_cast<E>(static_cast<underlying_type>(lhs) &
+                          static_cast<underlying_type>(rhs));
+}
+
+template <typename E>
+requires(EnableBitmaskOperations<E>::enable) inline constexpr E operator^(E lhs,
+                                                                          E rhs)
+{
+    using underlying_type = typename std::underlying_type<E>::type;
+    return static_cast<E>(static_cast<underlying_type>(lhs) ^
+                          static_cast<underlying_type>(rhs));
+}
+
+template <typename E>
+requires(EnableBitmaskOperations<E>::enable) inline constexpr E &operator|=(
+    E &lhs, E rhs)
+{
+    return lhs = lhs | rhs;
+}
+
+template <typename E>
+requires(EnableBitmaskOperations<E>::enable) inline constexpr E &operator&=(
+    E &lhs, E rhs)
+{
+    return lhs = lhs & rhs;
+}
+
+template <typename E>
+requires(EnableBitmaskOperations<E>::enable) inline constexpr E &operator^=(
+    E &lhs, E rhs)
+{
+    return lhs = lhs ^ rhs;
+}
+
+template <typename E>
+requires(EnableBitmaskOperations<E>::enable) inline constexpr E operator~(E e)
+{
+    return static_cast<E>(
+        ~static_cast<typename std::underlying_type<E>::type>(e));
 }
